1b2bf1616a0966bd4027ac104033b237a9ceabe0
Daniel Deutsch <deudan1010gmail.com>
2016-11-20 17:49:56 +0100

    connecting successful to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var express=require("express");
 var app=express();
 var port=process.env.PORT||3000;
+var fs=require("fs");
+var path=require("path");
+var mongodb = require("mongodb");//import mongodb drivers
+var mongoose=require("mongoose");//import mongodb client
+
+mongoose.connect("mongodb://test:test@ds159387.mlab.com:59387/urlshortener");//connect to mongodb
+mongoose.connection.on("error", function(err){
+  console.log("MongoDB failed to connect because"+err);
+  process.exit(-1);
+});//exit process and display error in case of error
+mongoose.connection.once("open", function(){
+  console.log("Connecting to MongoDB successful!");
+})
+
+
+
 
 app.get("/", function(req, res){
   res.send("it works!");
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
-URL Shortener Microservice
-A Free Code Camp Challenge
-Written by Daniel Deutsch
+#URL Shortener Microservice
+###A Free Code Camp Challenge
+###Written by Daniel Deutsch
 
-This Version works on heroku!
+**This Version works on heroku!**
